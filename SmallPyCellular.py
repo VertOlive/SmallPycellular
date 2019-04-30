c2600d84546f21cf8ecda02b5d56fa837fc42870
VertOlive <vertolive13@gmail.com>
2019-05-01 00:40:14 +0200

    Working Stage

--- SmallPyCellular.py
+++ SmallPyCellular.py
@@ -1,21 +1,32 @@
 import numpy as np
-import png
+from PIL import Image
 
-WOLFRAMRULE = 90
-RESOLUTIONSIZE = 2*2
-IMAGESIZE = 50
+WOLFRAMRULE = np.uint8(90)
+RESOLUTIONSIZE = 4
+IMAGESIZE = 100
 
 def convertToPng(numpyList):
-    pass ## TODO: Work in progress.
+    size = numpyList.shape[::-1]
+    databytes = np.packbits(numpyList, axis=1)
+    Image.frombytes(mode='1', size=size, data=databytes).save("Cellular.png")
 
 def main():
-    image = np.zeros(np.shape(IMAGESIZE,IMAGESIZE), dtype=bool)
-    image[0][(int)(IMAGESIZE/2)] = true
+    ## Unpack bits rules.
+    rulesList = np.unpackbits(WOLFRAMRULE)
+    print(rulesList)
+    ## Make blank cellular field buffer with one active cell.
+    cellularField = np.zeros((IMAGESIZE, IMAGESIZE), dtype=bool)
+    cellularField[0][int(IMAGESIZE/2) -1] = True
 
-    for(y in range(0, IMAGESIZE)):
-        for(x in range(0, IMAGESIZE)):
-            pass ## TODO: Work in progress.
+    ## Process rules
+    for y in range(0, IMAGESIZE-1):
+        for x in range(1, IMAGESIZE-1):
+            value = int(cellularField[y][x-1])*1 + int(cellularField[y][x])*2 + int(cellularField[y][x+1])*4
+            if(rulesList[value] == 1):
+                cellularField[y+1][x] = True
 
+
+    convertToPng(cellularField)
 
 if(__name__ == "__main__"):
     main()
