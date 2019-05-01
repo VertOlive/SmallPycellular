af5db6b4a29a8ab4f9e7aac63d315fb588c6c1f1
VertOlive <vertolive13@gmail.com>
2019-05-01 13:36:50 +0200

    Fonctionnal Code

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # SmallPycellular
 ## Description
 1 dimensional cellular automata made with Python3.
-Create a snapshot of the evolution of this cellular automata.
+Create a image of the evolution of one dimensional cellular automata.
+
+## Dependancies
+This small python3 program use the package Numpy.
--- SmallPyCellular.py
+++ SmallPyCellular.py
@@ -1,7 +1,17 @@
 import numpy as np
+import argparse
 from PIL import Image
 
-WOLFRAMRULE = np.uint8(90)
+parser = argparse.ArgumentParser()
+parser.add_argument("rule",
+                    type=np.uint8,
+                    help="Set the Wolfram rule from 0 to 255")
+parser.add_argument("-r", "--random",
+                    action="store_true",
+                    help="Set ramdom patern of initial cells")
+args = parser.parse_args()
+
+WOLFRAMRULE = np.uint8(args.rule)
 RESOLUTIONSIZE = 4
 IMAGESIZE = 100
 
@@ -14,18 +24,25 @@ def main():
     ## Unpack bits rules.
     rulesList = np.unpackbits(WOLFRAMRULE)
     print(rulesList)
-    ## Make blank cellular field buffer with one active cell.
+    rulesList= np.flip(rulesList)
+    ## Make blank cellular field buffer..
     cellularField = np.zeros((IMAGESIZE, IMAGESIZE), dtype=bool)
-    cellularField[0][int(IMAGESIZE/2) -1] = True
+
+
+    if(args.random == True):
+        for i in range(0, IMAGESIZE):
+            cellularField[0][i] = bool(np.random.randint(0,2))
+    else:
+        cellularField[0][int(IMAGESIZE/2) -1] = True
 
     ## Process rules
     for y in range(0, IMAGESIZE-1):
         for x in range(1, IMAGESIZE-1):
-            value = int(cellularField[y][x-1])*1 + int(cellularField[y][x])*2 + int(cellularField[y][x+1])*4
+            value = int(cellularField[y][x-1])*4 + int(cellularField[y][x])*2 + int(cellularField[y][x+1])*1
             if(rulesList[value] == 1):
                 cellularField[y+1][x] = True
 
-
+    ## Create the image from the bufferd cellularField.
     convertToPng(cellularField)
 
 if(__name__ == "__main__"):
